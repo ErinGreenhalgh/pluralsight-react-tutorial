47935b3e511abed40f8fad4b6bc040a74ba4d1e8
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-21 18:22:32 -0700

    fire off the call to the api from the root of our project: we call the dispatch method on our store and pass it the function from our thunk, making sure to invoke the function

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
-
+store.dispatch(loadCourses());
+//make sure that you are calling the API call function 
 render (
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
