6fbd6e122b539e731a5258b386f8b7745ac3a0c0
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-21 18:12:24 -0700

    add first thunk and change action type to LOAD_COURSE_SUCCESS based on action type naming conventions

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,21 @@
 import * as types from './actionTypes';
+import courseApi from './api/mockCourseApi'
 
-export function createCourse(course) {
-return {type: types.CREATE_COURSE, course};
+export function loadCoursesSuccess(course) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+
+//thunk:
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+    //you can make an ajax call right here in the function
+    //but it's better to abstract that logic somewhere else so that it's simple to change
+    //how you're accessing data if you want to later on
+  }
 }
