a3eed0a8f9f6c641e9ca5b25c2d35c83d847439c
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-12-21 17:00:23 -0700

    fix some linting errors and delete comments

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import 'babel-polyfill';
-//need a polyfill for some features in ES6 that babel can't transpile, like Objet.assign
-//you can import separate polyfills as needed to
 import React from 'react';
 import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
-//a higher order component that attaches the store to our container components
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const store = configureStore(); //could pass an intial state here, esp if you were creating a server-rendered app
+const store = configureStore();
 
 render (
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
-
 );
 
 //provider takes store as a prop and wraps our provider component
